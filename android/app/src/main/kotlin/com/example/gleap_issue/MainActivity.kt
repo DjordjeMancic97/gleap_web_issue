0d055e7ba0ff5f3b37f8da237206dc45be022e3f
Djordje Mancic <dmancic@gsmorg.com>
2025-04-02 14:37:37 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gleap_issue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gleap_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:gleap_sdk/gleap_sdk.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      // if (!kIsWeb) await Gleap.enableDebugConsoleLog();
+      // Gleap.setLanguage(language: const Locale('en').languageCode);
+      await Gleap.initialize(token: 'Add your own');
+      await Gleap.showFeedbackButton(true);
+    });
+  }
+
+  void _incrementCounter() {
+    Gleap.open();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[const Text('You have pushed the button this many times:'), Text('Gleap test', style: Theme.of(context).textTheme.headlineMedium)],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
